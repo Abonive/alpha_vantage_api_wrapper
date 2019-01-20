1d555180aa888438ebbf7fd0436893aa58f836d3
Damian.K <damian.k@viccmedia.es>
2019-01-20 03:09:35 +0100

    index removed

--- index.js
+++
@@ -1,8 +0,0 @@
-let Alpha = require('./lib/index').Alpha
-
-const a = new Alpha('REE8808MDBQ589HQ')
-
-a.stocks.search('BBVA').then((res) => {
-  // console.log(res["Meta Data"]["2. Symbol"]);
-  console.log(res);
-})
